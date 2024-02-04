b13e47dced2828b4b35c87183f3f9ba0cbd33df3
hyungyeom <rlagusrua3687@gmail.com>
2024-02-04 14:01:16 +0900

    chore: initial setting

---
+++ src/main/java/com/openaiSpring/openaiSpring/OpenaiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.openaiSpring.openaiSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenaiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenaiSpringApplication.class, args);
+	}
+
+}
